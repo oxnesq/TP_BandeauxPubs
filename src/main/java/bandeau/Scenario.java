5bde8676d4494973edb263b78d678dcd1d68ecab
oceane <oceane.copub@gmail.com>
2024-10-09 10:11:49 +0200

    end

--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -1,6 +1,11 @@
 package bandeau;
+
 import java.util.List;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Classe utilitaire pour représenter la classe-association UML
@@ -15,22 +20,26 @@ class ScenarioElement {
         repeats = r;
     }
 }
+
 /**
  * Un scenario mémorise une liste d'effets, et le nombre de repetitions pour chaque effet
  * Un scenario sait se jouer sur un bandeau.
  */
-public class Scenario {
+public class Scenario extends Thread {
 
     private final List<ScenarioElement> myElements = new LinkedList<>();
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     /**
      * Ajouter un effect au scenario.
      *
-     * @param e l'effet à ajouter
+     * @param e       l'effet à ajouter
      * @param repeats le nombre de répétitions pour cet effet
      */
-    public void addEffect(Effect e, int repeats) {
+    public void addEffect(Effect e, int repeats){
+        readWriteLock.writeLock().lock();
         myElements.add(new ScenarioElement(e, repeats));
+        readWriteLock.writeLock().unlock();
     }
 
     /**
@@ -38,11 +47,19 @@ public class Scenario {
      *
      * @param b le bandeau ou s'afficher.
      */
-    public void playOn(Bandeau b) {
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(b);
-            }
-        }
+    public void playOn(BandeauVerrouillable b) {
+        Thread thread = new Thread(() -> {
+                b.lock();
+                readWriteLock.readLock().lock();
+                for (ScenarioElement element : myElements) {
+                    for (int repeats = 0; repeats < element.repeats; repeats++) {
+                        element.effect.playOn(b);
+                    }
+                }
+                readWriteLock.readLock().unlock();
+                b.unlock();
+        });
+        thread.start();
+
     }
 }
